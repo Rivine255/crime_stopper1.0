876e1e6b29998ba838c00c4e699af429910d1250
Noel Kapungu <59761792+DePosta20@users.noreply.github.com>
2020-07-01 22:12:59 +0300

    Add files via upload

--- anonymous/anonymous.php
+++ anonymous/anonymous.php
@@ -47,10 +47,10 @@ if ($result_ward->num_rows > 0) {
 	<meta name="description" content="website description" />
 	<meta name="keywords" content="website keywords, website keywords" />
 	<meta http-equiv="content-type" content="text/html; charset=windows-1252" />
-	<link rel="stylesheet" type="text/css" href="../css/style_.css" />
+	<link rel="stylesheet" type="text/css" href="../css/style.css" />
 	<link rel="stylesheet" type="text/css" href="../css/font-awesome.min.css" />
 	<link rel="stylesheet" type="text/css" href="../css/button.css" />
-	<link rel="shortcut icon" type="image/png" href="../img/favicon.png">
+	<link rel="shortcut icon" type="image/x-icon" href="../img/logo.ico" />
 </head>
 
 <body>
@@ -143,7 +143,6 @@ if ($result_ward->num_rows > 0) {
 				</div>
 			</div>
 		</div>
-		<div id="content_footer"></div>
 		<div id="footer">
 			<p>Copyright &copy; Rivine <?php echo date("Y");?> </p>
 		</div>
