5eaeadf1fdca7eedfcd5c5364b97808d6240dd47
Noel Kapungu <59761792+DePosta20@users.noreply.github.com>
2020-07-22 12:03:20 +0300

    Update _crime.js
    Changes make when police want to change the progress of the case from NOT YET to either ACCOMPLISHED or ON PROGRESS

--- js/_crime.js
+++ js/_crime.js
@@ -17,3 +17,17 @@ function makeSticky() {
        navbar.classList.remove("sticky");
     }
 }
+
+function validate_progress() {
+    var select = document.getElementById("select");
+    var xx = select.value;
+    var yy = document.getElementById("wait");
+    if (xx == "NOT YET") {
+        yy.innerHTML = "Please, Select the further steps That takes place";
+        select.style.borderColor = "red";
+    } else {
+        yy.innerHTML = "";
+        select.style.borderColor = "#ccc";
+        return true;
+    }
+}
