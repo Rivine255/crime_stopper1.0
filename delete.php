5fcb382c1475d484fd60cabb86b783dc113af720
Dion Kapfumvuti <dionkapf@gmail.com>
2020-07-24 00:29:44 +0300

    Fix formatting

--- delete.php
+++ delete.php
@@ -1,13 +1,11 @@
 <?php
 include('../functions.php');
-$id=$_REQUEST['id'];
-if(mysqli_connect_errno()){
-    echo "Filed to connect".mysqli_connect_error();
+$id = $_REQUEST['id'];
+if (mysqli_connect_errno()) {
+    echo "Filed to connect" . mysqli_connect_error();
 }
-if(isset($id)){
-$query = "DELETE FROM users WHERE id=$id"; 
-$result = mysqli_query($db,$query);
-header("location: edit.php");
-
+if (isset($id)) {
+    $query = "DELETE FROM users WHERE id=$id";
+    $result = mysqli_query($db, $query);
+    header("location: edit.php");
 }
-?>
