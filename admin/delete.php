88290d1dfa24b5f98ca99fc0282e025fde59fced
Kefa12 <67233299+Kefa12@users.noreply.github.com>
2020-07-26 12:03:38 +0300

    add delete.php
    file to delete user

---
+++ admin/delete.php
@@ -0,0 +1,13 @@
+<?php
+include('../functions.php');
+$id=$_REQUEST['id'];
+if(mysqli_connect_errno()){
+    echo "Filed to connect".mysqli_connect_error();
+}
+if(isset($id)){
+$query = "DELETE FROM users WHERE id=$id"; 
+$result = mysqli_query($db,$query) or die(mysqli_error());
+header("location: edit.php");
+
+}
+?>
