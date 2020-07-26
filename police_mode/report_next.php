b995fd644fb279da534263b5cce80824c96a82d0
Noel Kapungu <59761792+DePosta20@users.noreply.github.com>
2020-07-26 13:17:11 +0300

    Update report_next.php

--- police_mode/report_next.php
+++ police_mode/report_next.php
@@ -54,7 +54,7 @@
 			<p>&nbsp;</p>
 		</div>
 		<!-- JAVASCRIPTS -->
-		<script type="text/javascript" src="js/_crime.js"></script>
+		<script type="text/javascript" src="../js/_crime.js"></script>
 	</body>
 
 </html>
