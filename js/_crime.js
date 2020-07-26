27c6922b1a100af4b82e5308e663f56bb434eab2
Noel Kapungu <59761792+DePosta20@users.noreply.github.com>
2020-07-26 20:34:17 +0300

    Add files via upload

--- js/_crime.js
+++ js/_crime.js
@@ -64,3 +64,25 @@ var span = document.getElementsByClassName("close")[0];
 span.onclick = function () {
     modal.style.display = "none";
 };
+
+///////////////////////////////////////////////////////////////////////////
+function search_in_table() {
+    // Declare variables
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("input");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("table");
+    tr = table.getElementsByTagName("tr");
+    // Loop through all table rows, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[0];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
