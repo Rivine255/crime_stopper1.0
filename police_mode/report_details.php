a68ee8ef3704f031806ed71c58618b51f3c7ccc4
Noel Kapungu <59761792+DePosta20@users.noreply.github.com>
2020-07-27 01:10:45 +0300

    Update report_details.php

--- police_mode/report_details.php
+++ police_mode/report_details.php
@@ -64,7 +64,7 @@ while ($row = $result_ward->fetch_assoc()) {
 <html>
 
 	<head>
-		<title>Anonymous Report</title>
+		<title>Report Details</title>
 		<meta name="description" content="website description" />
 		<meta name="keywords" content="website keywords, website keywords" />
 		<meta http-equiv="content-type" content="text/html; charset=windows-1252" />
