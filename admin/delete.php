46b698e1840738f4c1cffbb8b3f1553f3bab1b22
Kefa12 <67233299+Kefa12@users.noreply.github.com>
2020-07-26 12:37:04 +0300

    add delete.php
    admin  delete user

--- admin/delete.php
+++ admin/delete.php
@@ -7,7 +7,7 @@ if(mysqli_connect_errno()){
 if(isset($id)){
 $query = "DELETE FROM users WHERE id=$id"; 
 $result = mysqli_query($db,$query) or die(mysqli_error());
-header("location: edit.php");
+header("location: home.php");
 
 }
 ?>
