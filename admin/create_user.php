9742b67eded354ca10f124a020d27b9c25e518aa
Dion Kapfumvuti <dionkapf@gmail.com>
2020-08-03 00:28:32 +0300

    ADD NEW USER ON ADMIN (INTERFACE) Fixes #15

--- admin/create_user.php
+++ admin/create_user.php
@@ -2,84 +2,92 @@
 <!DOCTYPE html>
 <html>
 
-	<head>
-		<title>Registration system PHP and MySQL - Create user</title>
-		<meta name="description" content="website description" />
-		<meta name="keywords" content="website keywords, website keywords" />
-		<meta http-equiv="content-type" content="text/html; charset=windows-1252" />
-		<link rel="stylesheet" type="text/css" href="../css/style.css"
-		<link rel="stylesheet" type="text/css" href="../css/button.css" />
-		<link rel="shortcut icon" type="image/x-icon" href="../img/logo.ico" />
-		<style>
-		button[name=register_btn] {
-			background: #003366;
-		}
-		</style>
-	</head>
+<head>
+	<title>Create a new user</title>
+	<meta name="description" content="website description" />
+	<meta name="keywords" content="website keywords, website keywords" />
+	<meta http-equiv="content-type" content="text/html; charset=windows-1252" />
+	<link rel="stylesheet" type="text/css" href="../css/style.css" <link rel="stylesheet" type="text/css" href="../css/button.css" />
+	<link rel="shortcut icon" type="image/x-icon" href="../img/logo.ico" />
+</head>
 
-	<body>
-		<div id="main">
-			<div id="header">
-				<div id="logo">
-					<div class="logo_img">
-						<img src="../img/logo.png" alt="NPS Logo" height="auto" width="13%"></img>
-						<div id="logo_text">
-							<!-- class="logo_colour", allows you to change the colour of the text -->
-							<h1><a class="logo_colour" href="../index.php">CrimeSTOPPERS</a></h1>
-							<h2>National Police Service</h2>
-						</div>
+<body>
+	<div id="main">
+		<div id="header">
+			<div id="logo">
+				<div class="logo_img">
+					<img src="../img/logo.png" alt="NPS Logo" height="auto" width="13%"></img>
+					<div id="logo_text">
+						<h1><a class="logo_colour" href="../index.php">CrimeSTOPPERS</a></h1>
+						<h2>National Police Service</h2>
 					</div>
-				</div>
-				<div id="navbar">
-					<nav>
-						<ul id="menu">
-							<li class="selected"><a href="home.php">Home</a></li>
-							<li style="float: right;"><a href="#">Contact Us</a></li>
-						</ul>
-					</nav>
 				</div>
 			</div>
-			<div id="site_content" class="content">
-				<div align="center" class="header">
-					<h1>New User</h1>
+
+			<div class="profile_info">
+				<figure class="user_avatar"><img src="../img/admin_profile.png" width="5%"></figure>
+				<div class="user_info">
+					<?php if (isset($_SESSION['user'])) : ?>
+						<strong><?php echo $_SESSION['user']['username']; ?></strong>
+						<small style="font-size:15px">
+							<i style="color: #888;">(<?php echo ucfirst($_SESSION['user']['user_type']); ?>)</i>
+							&nbsp;
+						</small>
+					<?php endif ?>
 				</div>
+			</div>
+
+			<div id="navbar">
+				<nav>
+					<ul id="menu">
+						<li class="selected"><a href="home.php">Home</a></li>
+						<li style="float: right;"><a href="#">Contact Us</a></li>
+					</ul>
+				</nav>
+			</div>
+		</div>
+		<div id="site_content" class="content">
+			<div align="center" class="header">
+				<h1>Create a New User</h1>
+			</div>
+			<div id="content">
 				<form method="post" action="create_user.php">
 					<?php echo display_error(); ?>
-					<div class="input-group">
-						<label>Username</label>
+
+					<div class="form_settings">
+						<p>Username</p>
 						<input type="text" name="username" value="<?php echo $username; ?>">
-					</div>
-					<div class="input-group">
-						<label>Email</label>
+
+						<p>Email</p>
 						<input type="email" name="email" value="<?php echo $email; ?>">
-					</div>
-					<div class="input-group">
-						<label>User type</label>
+
+						<p>User type</p>
 						<select name="user_type" id="user_type">
 							<option value="">--choose role--</option>
 							<option value="admin">Admin</option>
 							<option value="police">Police</option>
 						</select>
-					</div>
-					<div class="input-group">
-						<label>Password</label>
+
+						<p>Password</p>
 						<input type="password" name="password_1">
-					</div>
-					<div class="input-group">
-						<label>Confirm password</label>
+
+						<p>Confirm password</p>
 						<input type="password" name="password_2">
-					</div>
-					<div class="input-group">
-						<button type="submit" class="btn" name="register_btn"> + Create user</button>
+						<p class="form_buttons">
+							<button type="submit" class="submit">CREATE USER</button>
+							<button type="reset" class="submit">RESET</button>
+						</p>
 					</div>
 				</form>
 			</div>
-			<div id="footer">
-				<a href="../login.php" style="color: red;">logout</a>
-				<br><br>
-				<p>Copyright &copy; Rivine <?php echo date("Y"); ?> All Rights Reserved.</p>
-			</div>
+
 		</div>
-	</body>
+		<div id="footer">
+			<a href="../login.php" style="color: red;">logout</a>
+			<br><br>
+			<p>Copyright &copy; Rivine <?php echo date("Y"); ?> All Rights Reserved.</p>
+		</div>
+	</div>
+</body>
 
 </html>
