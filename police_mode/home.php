48c95005fd8ea7c96604c082fe9aa479f953653c
Noel Kapungu <noelikapungu3@gmail.com>
2020-08-03 19:47:16 +0300

    Update home.php

--- police_mode/home.php
+++ police_mode/home.php
@@ -14,6 +14,7 @@ $username = $_SESSION['user']['username'];
 		<link rel="stylesheet" type="text/css" href="../css/style.css" />
 		<link rel="stylesheet" type="text/css" href="../css/button.css" />
 		<link rel="shortcut icon" type="image/x-icon" href="../img/logo.ico" />
+		<script src="https://kit.fontawesome.com/bf523026c9.js" crossorigin="anonymous"></script>
 	</head>
 
 	<body>
@@ -28,62 +29,63 @@ $username = $_SESSION['user']['username'];
 						</div>
 					</div>
 				</div>
-				<div class="profile_info">
-					<figure class="user_avatar"><img src="../img/admin_profile.png" width="5%">
-						<span style="float: right;">
-							<a href="../chat_box/chat_list.php"><i class="fa fa-2x fa-envelope"></i></a>
-						</span>
-					</figure>
-					<div class="user_info">
-						<?php if (isset($_SESSION['user'])) : ?>
-						<strong><?php echo $_SESSION['user']['username']; ?></strong>
-						<small style="font-size:15px">
-							<i style="color: #888;">(<?php echo ucfirst($_SESSION['user']['user_type']); ?>)</i>
-							&nbsp;
-						</small>
-						<?php endif ?>
-					</div>
+			</div>
+			<div class="profile_info">
+				<figure class="user_avatar"><img src="../img/admin_profile.png" width="5%">
+					<span style="float: right;">
+						<a href="../chat_box/chat_list.php"><i class="fa fa-2x fa-envelope"></i></a>
+					</span>
+				</figure>
+				<div class="user_info">
+					<?php if (isset($_SESSION['user'])) : ?>
+					<strong><?php echo $_SESSION['user']['username']; ?></strong>
+					<small style="font-size:15px">
+						<i style="color: #888;">(<?php echo ucfirst($_SESSION['user']['user_type']); ?>)</i>
+						&nbsp;
+					</small>
+					<?php endif ?>
 				</div>
+			</div>
 
-				<div id="navbar">
-					<nav>
-						<ul id="menu">
-							<li class="selected"><a href="home.php">Home</a></li>
-							<li><a href="report.php">Report</a></li>
-						</ul>
-					</nav>
-				</div>
+			<div id="navbar">
+				<nav>
+					<ul id="menu">
+						<li class="selected"><a href="home.php">Home</a></li>
+						<li><a href="report.php">Report</a></li>
+					</ul>
+				</nav>
 			</div>
-			<div id="site_content" class="content">
-				<div align="center" class="header">
-					<h1>Home</h1>
-				</div>
-				<div id="wait"></div>
-				<div id="content">
-					<h4>
-						<?php
-					$time = date("H");
-					if (($time <= 11) and ($time >= 5)) {
-						echo "Good Morning $username,";
-					} elseif (($time <= 16) and ($time >= 12)) {
-						echo "Good Afternoon $username,";
-					} elseif (($time <= 20) and ($time >= 17)) {
-						echo "Good Evening $username,";
-					} else {
-						echo "Good night $username,";
-					} ?>
-					</h4>
+		</div>
+		<div id="site_content" class="content">
+			<div align="center" class="header">
+				<h1>Home</h1>
+			</div>
+			<div id="wait"></div>
+			<div id="content">
+				<h4>
+					<?php
+				$time = date("H");
+				if (($time <= 11) and ($time >= 5)) {
+					echo "Good Morning $username,";
+				} elseif (($time <= 16) and ($time >= 12)) {
+					echo "Good Afternoon $username,";
+				} elseif (($time <= 20) and ($time >= 17)) {
+					echo "Good Evening $username,";
+				} else {
+					echo "Good night $username,";
+				} ?>
+				</h4>
 
-				</div>
 			</div>
-			<div id="footer">
-				<a href="../login.php" id="logout">logout</a>
-				<br><br>
-				<p>Copyright &copy; Rivine <?php echo date("Y"); ?> All Rights Reserved.</p>
-			</div>
-			<!-- JAVASCRIPTS -->
-			<script type="text/javascript" src="js/ajax.js"></script>
-			<script type="text/javascript" src="js/_crime.js"></script>
+		</div>
+		<div id="footer">
+			<a href="../login.php" id="logout">logout</a>
+			<br><br>
+			<p>Copyright &copy; Rivine <?php echo date("Y"); ?> All Rights Reserved.</p>
+		</div>
+		<!-- JAVASCRIPTS -->
+		<script type="text/javascript" src="js/ajax.js"></script>
+		<script type="text/javascript" src="js/_crime.js"></script>
 	</body>
 
 </html>
